36cd7cf8236f66819b3f59b4371f1daf5db89dac
Cong <congli35@gmail.com>
2026-01-09 23:50:37 +0800

    update trigger.dev node version

--- packages/tasks/trigger.config.ts
+++ packages/tasks/trigger.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
 	project: "proj_nwrjmmlvaqcsgsxcysot",
-	runtime: "node",
+	runtime: "node-22",
 	tsconfig: "./tsconfig.json",
 	logLevel: "log",
 	maxDuration: 300,
